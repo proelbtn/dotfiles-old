f84b7a05857ad59dcfb5bb15293e210854cc7e48
proelbtn <proelbtn@users.noreply.github.com>
2019-01-31 10:12:51 +0900

    add command "list"

---
+++ dottt.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+get_recipes() {
+    if test $# -ne 0
+    then
+        return 1
+    fi
+
+    if test "$1" = "all"
+    then
+        ls ./recipes
+    elif stat ./recipes/"$1"
+    then
+        echo $1
+    else
+        return 1
+    fi
+}
+
+list_recipes() {
+    RECIPES=$(command ls recipes)
+
+    echo ${RECIPES}
+}
+
+install_recipes() {
+    true
+}
+
+usage() {
+    echo "Usage: ${0} command [argments...]"
+    echo ""
+    echo "Available Commands:"
+    echo "  install   Installs files from recipes."
+    echo "  help      Prints usages."
+    echo "  list      Lists recipes."
+}
+
+case $1 in
+    install ) 
+        shift; install_recipes $@
+        ;;
+    list ) 
+        shift; list_recipes $@
+        ;;
+    help ) usage ;;
+    * ) 
+        echo "Error: can't recognize command '$1'"
+        usage 
+        ;;
+esac
