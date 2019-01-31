f5b7b3fc8628e2a03dac6fd871aa9a8bbf359af6
proelbtn <proelbtn@users.noreply.github.com>
2019-01-31 17:20:18 +0900

    change the priority in which local commands are executed

--- recipes/zsh/files/main.zsh
+++ recipes/zsh/files/main.zsh
@@ -1,6 +1,6 @@
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
-export PATH="${PATH}:${XDG_CONFIG_HOME}/zsh/bin"
+export PATH="${XDG_CONFIG_HOME}/zsh/bin:${PATH}"
 
 if test "$(ls "${XDG_CONFIG_HOME}/zsh/sources")" != ""
 then
