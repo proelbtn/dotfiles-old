ca88486779f7e6a78559fc23320157a6d8cf2769
proelbtn <proelbtn@users.noreply.github.com>
2019-02-04 00:40:06 +0900

    add color settings for ls

--- recipes/zsh/files/main.zsh
+++ recipes/zsh/files/main.zsh
@@ -1,6 +1,22 @@
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
+is_osx() {
+    uname -a | grep "Darwin" >/dev/null 2>&1
+    return $?
+}
+
+is_linux() {
+    uname -a | grep "Linux" >/dev/null 2>&1
+    return $?
+}
+
 export PATH="${XDG_CONFIG_HOME}/zsh/bin:${PATH}"
+
+if is_linux
+then
+    alias ls="ls --color"
+fi
+
 
 if test "$(ls "${XDG_CONFIG_HOME}/zsh/sources")" != ""
 then
