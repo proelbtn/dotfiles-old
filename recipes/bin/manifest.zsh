56f976e0f4eb632adb2bee4a863d5623c6908837
proelbtn <proelbtn@users.noreply.github.com>
2019-02-25 15:11:44 +0900

    fix bin's manifest.zsh

--- recipes/bin/manifest.zsh
+++ recipes/bin/manifest.zsh
@@ -5,6 +5,6 @@ install() {
 
     for file in "$(ls "${DOTTT_ROOT}/files")"
     do
-        ln -sf "${DOTTT_ROOT}/files/${file}" "${XDG_CONFIG_HOME}/zsh/bin/${file}"
+        ln -sf "${DOTTT_ROOT}/files/${file}" "${XDG_CONFIG_HOME}/zsh/bin/"
     done
 }
