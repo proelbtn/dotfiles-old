8d4a3b1997bf7abed823535583f0c9ddcee4ee42
proelbtn <proelbtn@users.noreply.github.com>
2019-03-10 10:39:33 +0900

    add comletions and syntax highlighting

--- recipes/zsh/files/main.zsh
+++ recipes/zsh/files/main.zsh
@@ -1,4 +1,3 @@
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
 is_osx() {
     uname -a | grep "Darwin" >/dev/null 2>&1
@@ -10,7 +9,20 @@ is_linux() {
     return $?
 }
 
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 export PATH="${XDG_CONFIG_HOME}/zsh/bin:${PATH}"
+
+source ~/.zplug/init.zsh
+
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+if ! zplug check --verbose
+then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
 
 if is_linux
 then
@@ -26,3 +38,5 @@ then
             && source "${XDG_CONFIG_HOME}/zsh/sources/${file}"
     done
 fi
+
+zplug load --verbose
--- recipes/zsh/manifest.zsh
+++ recipes/zsh/manifest.zsh
@@ -1,5 +1,8 @@
+ZSH_ZPLUG_INSTALLER_URL="https://raw.githubusercontent.com/zplug/installer/master/installer.zsh"
+
 install() {
     info "Installing zsh..."
     mkdir -p ${XDG_CONFIG_HOME}/zsh/{bin,sources}
     ln -sf "${DOTTT_ROOT}/files/main.zsh" ~/.zshrc
+    curl -sL --proto-redir -all,https "${ZSH_ZPLUG_INSTALLER_URL}" | zsh
 }
