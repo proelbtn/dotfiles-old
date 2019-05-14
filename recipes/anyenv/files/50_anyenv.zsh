48203123c015c986c2ff7f7fbb00dbde9bc785f7
proelbtn <proelbtn@users.noreply.github.com>
2019-05-15 00:28:36 +0900

    fix source file to work anyenv properly

--- recipes/anyenv/files/50_anyenv.zsh
+++ recipes/anyenv/files/50_anyenv.zsh
@@ -1,1 +1,8 @@
-export PATH="${PATH}"
+export PATH="${XDG_CONFIG_HOME}/anyenv/anyenv/bin:${PATH}"
+
+if [ ! -d "${XDG_CONFIG_HOME}/anyenv/anyenv-install" ]
+then
+    yes | anyenv install --init
+fi
+
+eval "$(anyenv init -)"
