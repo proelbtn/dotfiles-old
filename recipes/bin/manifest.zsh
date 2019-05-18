e08071fa7e49b57821175ccf90204ec040e2116e
proelbtn <proelbtn@users.noreply.github.com>
2019-05-19 00:20:04 +0900

    fix that ${XDG_CONFIG_HOME}/zsh/bin/files is created

--- recipes/bin/manifest.zsh
+++ recipes/bin/manifest.zsh
@@ -3,7 +3,7 @@ DEPENDS_ON="zsh"
 install() {
     info "Installing utilities..."
 
-    for file in "$(ls "${DOTTT_ROOT}/files")"
+    for file in $(ls "${DOTTT_ROOT}/files")
     do
         ln -sf "${DOTTT_ROOT}/files/${file}" "${XDG_CONFIG_HOME}/zsh/bin/"
     done
