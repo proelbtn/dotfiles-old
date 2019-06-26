be9d30c83e0ab920aa57ca8cb8cf0f647d6f0793
proelbtn <proelbtn@users.noreply.github.com>
2019-06-27 02:07:57 +0900

    add rustup recipe

---
+++ recipes/commands/rustup/manifest.zsh
@@ -0,0 +1,13 @@
+DEPENDS_ON="essentials/zsh"
+
+install() {
+    info "Installing rustup..."
+
+    local TEMPDIR="$(mktemp -d)"
+
+    curl https://sh.rustup.rs > "${TEMPDIR}/rustup-init.sh"
+
+    sh "${TEMPDIR}/rustup-init.sh" -y
+
+    ln -sf "${DOTTT_ROOT}/files/50_anyenv.zsh" ${XDG_CONFIG_HOME}/zsh/sources
+}
