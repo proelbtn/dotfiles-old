fe6d80b998329ea1b60671a203880f2266382e2e
proelbtn <proelbtn@users.noreply.github.com>
2019-12-12 17:09:00 +0900

    add environment file

---
+++ makefiles/environments.mk
@@ -0,0 +1,9 @@
+CWD := $(shell pwd)
+
+XDG_CACHE_HOME ?= ${HOME}/.cache
+
+XDG_CONFIG_HOME ?= ${HOME}/.config
+XDG_CONFIG_DIRS ?= /etc/xdg
+
+XDG_DATA_HOME ?= ${HOME}/.local/share
+XDG_DATA_DIRS ?= /usr/local/share:/usr/share
