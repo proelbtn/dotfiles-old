23db9111b5b7dfcd85df87dc6b2df081c0753df5
proelbtn <proelbtn@users.noreply.github.com>
2019-12-16 21:25:17 +0900

    add git shortcuts

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -58,10 +58,14 @@ is_linux && alias paste="xclip -o -selection c"
 
 alias gia="git add"
 alias giaa="git add ."
+alias giap="git add -p ."
 alias gic="git commit"
+alias gica="git commit --amend"
+alias gicane="git commit --amend"
 alias gii="git init"
 alias gip="git push"
 alias gipo="git push origin"
+alias gipoa="git push origin --all"
 alias gipom="git push origin master"
 
 ###
