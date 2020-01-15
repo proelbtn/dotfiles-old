adcac9ee55605bde64607ff4f84cb12ea3a3f302
proelbtn <proelbtn@users.noreply.github.com>
2020-01-15 12:58:58 +0900

    fix ANYENV_ROOT is not set and enable fast loading for anyenv

--- anyenv/50_anyenv.zsh
+++ anyenv/50_anyenv.zsh
@@ -1,8 +1,32 @@
-export PATH="${XDG_CONFIG_HOME}/anyenv/bin:${PATH}"
+export ANYENV_ROOT="${XDG_CONFIG_HOME}/anyenv"
+export PATH="${ANYENV_ROOT}/bin:${PATH}"
 export GOENV_GOPATH_PREFIX="${HOME}/.local/go"
 
-[ ! -d "${XDG_CONFIG_HOME}/anyenv/anyenv-install" ] && yes | anyenv install --init || true
-eval "$(anyenv init -)"
+[ ! -d "${ANYENV_ROOT}/anyenv-install" ] && yes | anyenv install --init || true
 
 export PATH="${PATH}:${GOPATH}/bin"
 
+# ===
+
+ENVS="$(command find ${XDG_CONFIG_HOME}/anyenv/envs/ -mindepth 1 -maxdepth 1 -type d)"
+
+BINS=""
+for env in $(echo ${ENVS}); do BINS="${BINS} $(command find ${env}/shims -mindepth 1 -type f)"; done
+
+FILES=""
+for env in $(echo ${ENVS}); do FILES="${FILES} $(basename ${env})"; done
+for bin in $(echo ${BINS}); do FILES="${FILES} $(basename ${bin})"; done
+
+TMPFILE="$(mktemp)"
+
+(
+for file in $(echo ${FILES}); do
+    echo "function ${file}() {"
+    echo "    eval \"\$(anyenv init -)\""
+    echo "    unset -f ${FILES}"
+    echo "    command ${file} \$@"
+    echo "}"
+  done
+) > ${TMPFILE}
+
+source ${TMPFILE}
