4f656314d7a6d23a4c757406fda74a242e5eba72
proelbtn <contact@proelbtn.com>
2020-02-24 03:13:36 +0900

    delete unnecessary files

--- fzf/50_fzf.zsh
+++ fzf/50_fzf.zsh
@@ -1,7 +1,2 @@
 alias fzf="fzf --height 40% --reverse"
 
-if which -s fzf-completion; then
-  export FZF_COMPLETION_TRIGGER=''
-  bindkey '^T' fzf-completion
-  bindkey '^I' $fzf_default_completion
-fi
