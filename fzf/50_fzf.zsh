af285778ffcf6a9e75c0d632b0e61f387a40faa3
proelbtn <contact@proelbtn.com>
2020-02-24 02:49:14 +0900

    modified fzf

--- fzf/50_fzf.zsh
+++ fzf/50_fzf.zsh
@@ -1,3 +1,7 @@
-if [ "${TMUX}" != "" ]; then
-  alias fzf="fzf-tmux"
+alias fzf="fzf --height 40% --reverse"
+
+if which -s fzf-completion; then
+  export FZF_COMPLETION_TRIGGER=''
+  bindkey '^T' fzf-completion
+  bindkey '^I' $fzf_default_completion
 fi
