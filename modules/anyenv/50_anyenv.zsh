bf72656abd65bd61bf5aedb863144ce3c19f6190
proelbtn <contact@proelbtn.com>
2020-05-16 17:30:03 +0900

    ANYENV_DEFINITION_ROOTを定義した

--- modules/anyenv/50_anyenv.zsh
+++ modules/anyenv/50_anyenv.zsh
@@ -1,8 +1,9 @@
 export ANYENV_ROOT="${XDG_DATA_HOME}/anyenv"
+export ANYENV_DEFINITION_ROOT="${ANYENV_ROOT}/anyenv-install"
 export PATH="${ANYENV_ROOT}/bin:${PATH}"
 export GOENV_GOPATH_PREFIX="${HOME}/.local/go"
 
-[ ! -d "${ANYENV_ROOT}/anyenv-install" ] && yes | anyenv install --init || true
+[ ! -d "${ANYENV_DEFINITION_ROOT}" ] && yes | anyenv install --init || true
 eval "$(anyenv init -)"
 
 export PATH="${PATH}:${GOPATH}/bin"
